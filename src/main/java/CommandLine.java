c5703296f766369d921dbe6202feed44ae11b910
katherinek07 <166860658+katherinek07@users.noreply.github.com>
2026-02-12 23:19:13 -0500

    katherine - commandline

--- src/main/java/CommandLine.java
+++ src/main/java/CommandLine.java
@@ -1,40 +1,55 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CommandLine {
+
+    private FileHandler fileHandler = new FileHandler();
+
     public void run(String[] args) {
-        ProgramController controller = new ProgramController();
+        File folder = new File("data");
+        File[] listOfFiles = folder.listFiles();
 
+        if (listOfFiles == null || listOfFiles.length == 0) {
+            System.out.println("No files found");
+            return;
+        }
+
+        Arrays.sort(listOfFiles, Comparator.comparing(File::getName));
+
+        //case 1: no args -> list files
         if (args.length == 0) {
-            controller.listFiles();
-            return;
-        }
-
-        if (args.length == 1) {
-            String fileNumber = args[0];
-
-            if (!isTwoDigitNumber(fileNumber)) {
-                printUsage();
-                return;
+            for (int i=0; i<listOfFiles.length; i++) {
+                System.out.printf("%02d %s%n", i+1, listOfFiles[i].getName());
             }
-
-            controller.displayFile(fileNumber, null);
             return;
         }
 
+        String fileNumber = args[0];
+
+        if (!isTwoDigitNumber(fileNumber)){
+            printUsage();
+            return;
+        }
+
+        int index = Integer.parseInt(fileNumber)-1;
+
+        if (index<0 || index>=listOfFiles.length){
+            System.out.println("Invalid file number");
+            return;
+        }
+
+        String filename = listOfFiles[index].getName();
+        String content = fileHandler.getData(fileName);
+
+        //case 3: cipher key
         if (args.length == 2) {
-            String fileNumber = args[0];
             String key = args[1];
-
-            if (!isTwoDigitNumber(fileNumber)) {
-                printUsage();
-                return;
-            }
-
-            controller.displayFile(fileNumber, key);
-            return;
         }
-        printUsage();
+        System.out.println(content);
     }
     private boolean isTwoDigitNumber(String s) {
-        return s != null && s.length() == 2 && Character.isDigit(s.charAt(0)) && Character.isDigit(s.charAt(1));
+        return s != null && s.length() == 2 && Character.isDigit(s.charAt(0)) && Character.isDigit(s.charAt(1)) && !s.equals("00");
     }
 
     private void printUsage() {
